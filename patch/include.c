606d7acd86058caf11019a4536f8741573ada3ac
mozkomor05 <mozkomor05@gmail.com>
2021-11-20 15:23:16 +0100

    initial commit

---
+++ patch/include.c
@@ -0,0 +1,29 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_ewmhtags.c"
+#include "bar_ltsymbol.c"
+#include "bar_powerline_status.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_systray.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "autostart.c"
+#include "cfacts.c"
+#include "cyclelayouts.c"
+#include "decorationhints.c"
+#include "dragcfact.c"
+#include "movestack.c"
+#include "pertag.c"
+#include "placemouse.c"
+#include "restartsig.c"
+#include "swapfocus.c"
+#include "togglefullscreen.c"
+#include "vanitygaps.c"
+#include "dragmfact.c"
+/* Layouts */
+#include "layout_grid.c"
+#include "layout_monocle.c"
